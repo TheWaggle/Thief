00c9f595b3dd216a38df1f3dca75ac8eaa1c8366
Yosuke Nakao <yoosukenakao@thewaggle.co.jp>
2019-01-20 19:54:14 +0900

    Mix exs correction

--- lib/thief.ex
+++ lib/thief.ex
@@ -1,31 +1,9 @@
 defmodule Thief do
+
   @moduledoc """
   Documentation for Thief.
   Scrape and get image link"
   """
-@description"""
-  Scrape and get image link.
-  """
-
-   def project do
-    [
-      app: :Thief,
-      version: "0.0.1",
-      elixir: "~> 1.0",
-      description: @description,
-      package: package(),
-      deps: deps()
-    ]
-    end
-
-def package do
-  [
-       maintainers: ["YOSUKENAKAO.me"],
-       licenses: ["MIT"],
-       links: %{"GitHub" => "https://github.com/TheWaggle/Thief.git"}
-  ]
-end
-
 
   @doc """
   your get link or images at URL.
@@ -34,7 +12,6 @@ end
 
       iex> Thief.steal( "https://xxxx.com" )
       [https://xxxx.com/img.png,https://xxxx.com/img.jpg]
-
 
 
 """
@@ -66,7 +43,6 @@ end
     { :error , _body, _request_url } -> "Domain check Error img"
     end
   end
-
 
   # urlからリンクを取得する
   def getlink( url ) do
--- mix.exs
+++ mix.exs
@@ -1,15 +1,33 @@
 defmodule Thief.MixProject do
   use Mix.Project
 
-  def project do
+   def project() do
     [
       app: :thief,
       version: "0.1.0",
-      elixir: "~> 1.7",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      elixir: "~> 1.0",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Thief",
+      source_url: "https://github.com/TheWaggle/Thief.git"
     ]
-  end
+    end
+
+    defp description() do
+      " This Project is Scrape and get image links."
+    end
+
+    defp package() do
+      [
+          name: "thief",
+          maintainers: ["YOSUKENAKAO.me"],
+          licenses: ["MIT"],
+          links: %{"GitHub" => "https://github.com/TheWaggle/Thief.git"}
+      ]
+    end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -24,8 +42,7 @@ defmodule Thief.MixProject do
       {:floki, "~> 0.20.4"},
       {:httpoison, "~> 1.5"},
       {:ex_doc, "~> 0.19.2", only: :dev},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:earmark, ">= 0.0.0"},
     ]
   end
 end
